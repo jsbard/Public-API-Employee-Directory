d07997914c5addc5d45f944c627452776ebf0759
Andrew Hickman <hello@andrewhickman.me>
2021-06-18 16:51:46 -0400

    Add ability to scroll through employees in modal
    Add functionality to the "previous" and "next" buttons in the modal.

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,7 @@ const url =`https://randomuser.me/api/?results=${numOfEmployees}&nat=us`;
 let gallery = document.getElementById("gallery");
 
 const getEmployeeData = async (url) => {
-    const employees = await fetch(url);
-    return employees;
+    return await fetch(url);
 }
 
 /** Populate gallery with employee data **/
@@ -43,24 +42,28 @@ const populateEmployees = (employeeData) => {
 const createModals = (employeeData) => {
     const body = document.body;
     const cards = document.getElementsByClassName("card");
+    let currentIndex;
     // Loop over all cards and add event listener for each
     for (let i=0; i<cards.length; i++) {
-        let employeeInfo = employeeData[i];
-        let image = employeeInfo.picture.large;
-        let firstName = employeeInfo.name.first;
-        let lastName = employeeInfo.name.last;
-        let bdayMonth = employeeInfo.dob.date.substr(5, 2);
-        let bdayDay = employeeInfo.dob.date.substr(8, 2);
-        let bdayYear = employeeInfo.dob.date.substr(0, 4);
-        let phone = employeeInfo.phone;
-        let email = employeeInfo.email;
-        let streetNum = employeeInfo.location.street.number;
-        let street = employeeInfo.location.street.name;
-        let city = employeeInfo.location.city;
-        let state = employeeInfo.location.state;
-        let zip = employeeInfo.location.postcode;
+        let html;
+
+        const generateMarkup = (employeeInfo) => {
+            let image = employeeInfo.picture.large;
+            let firstName = employeeInfo.name.first;
+            let lastName = employeeInfo.name.last;
+            let bdayMonth = employeeInfo.dob.date.substr(5, 2);
+            let bdayDay = employeeInfo.dob.date.substr(8, 2);
+            let bdayYear = employeeInfo.dob.date.substr(0, 4);
+            let phone = employeeInfo.phone;
+            let email = employeeInfo.email;
+            let streetNum = employeeInfo.location.street.number;
+            let street = employeeInfo.location.street.name;
+            let city = employeeInfo.location.city;
+            let state = employeeInfo.location.state;
+            let zip = employeeInfo.location.postcode;
+
         // Modal markup template
-        let html = `
+            html = `
                 <div class="modal-container">
                     <div class="modal">
                         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
@@ -82,25 +85,82 @@ const createModals = (employeeData) => {
                 </div>
             `;
 
-        cards[i].addEventListener("click", (e) => {
+            return html;
+        }
 
+        // Display corresponding employee when their card is clicked
+        cards[i].addEventListener("click", (e) => {
+            let employeeInfo = employeeData[i];
+            // Marker to keep track of current modal
+            // utilized in addPrevButtonListener() and addNextButtonListener()
+            currentIndex = i;
+            //Insert clicked employee's modal into the DOM
+            generateMarkup(employeeInfo);
             body.insertAdjacentHTML("beforeend", html);
 
-            /** EVENT LISTENERS FOR MODAL CONTENT **/
+            // Call listeners on modal buttons
             const modal = document.getElementsByClassName("modal-container")[0];
             const close = document.getElementById("modal-close-btn");
             const prev = document.getElementById("modal-prev");
             const next = document.getElementById("modal-next");
 
-            close.addEventListener("click", () => {
+            addCloseModalListener(close, modal);
+            addPrevButtonListener(prev, modal, i, employeeData);
+            addNextButtonListener(next, modal, i, employeeData);
+        });
 
+        /** EVENT LISTENERS FOR MODAL CONTENT **/
+        const addCloseModalListener = (button, modal) => {
+            button.addEventListener("click", () => {
                 modal.parentNode.removeChild(modal);
             });
+        }
 
-            prev.addEventListener("click", () => {
-            });
+        const addPrevButtonListener = (button, modal, index, employeeData) => {
+            if (index > 0) {
+                button.addEventListener("click", () => {
+                    const i = index - 1;
+                    const html = generateMarkup(employeeData[i]);
+                    modal.parentNode.removeChild(modal);
+                    body.insertAdjacentHTML("beforeend", html);
 
-        });
+                    // Add listeners to the newly generated modal buttons
+                    const newCloseButton = document.getElementById("modal-close-btn");
+                    const newModal = document.getElementsByClassName("modal-container")[0];
+                    const newPrevButton = document.getElementById("modal-prev");
+                    const newNextButton = document.getElementById("modal-next");
+                    addPrevButtonListener(newPrevButton, newModal, i, employeeData);
+                    addNextButtonListener(newNextButton, newModal, i, employeeData);
+                    addCloseModalListener(newCloseButton, newModal);
+                });
+            } else { // Remove Previous button if there is no previous employee
+                const prevButton = document.getElementById("modal-prev");
+                prevButton.parentNode.removeChild(prevButton);
+            }
+        }
+
+        const addNextButtonListener = (button, modal, index, employeeData) => {
+            if (index < employeeData.length-1) {
+                button.addEventListener("click", () => {
+                    const i = index + 1;
+                    const html = generateMarkup(employeeData[i]);
+                    modal.parentNode.removeChild(modal);
+                    body.insertAdjacentHTML("beforeend", html);
+
+                    // Add listeners to the newly generated modal buttons
+                    const newCloseButton = document.getElementById("modal-close-btn");
+                    const newModal = document.getElementsByClassName("modal-container")[0];
+                    const newPrevButton = document.getElementById("modal-prev");
+                    const newNextButton = document.getElementById("modal-next");
+                    addPrevButtonListener(newPrevButton, newModal, i, employeeData);
+                    addNextButtonListener(newNextButton, newModal, i, employeeData);
+                    addCloseModalListener(newCloseButton, newModal);
+                });
+            } else { // Remove Next button if there is no next employee
+                const nextButton = document.getElementById("modal-next");
+                nextButton.parentNode.removeChild(nextButton);
+            }
+        }
     }
 }
 
